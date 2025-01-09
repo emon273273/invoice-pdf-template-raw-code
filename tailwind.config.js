a7696b05c9b14f61e475bdcf7ada4bb32a654030
emon273273 <emon.cse273@gmail.com>
2025-01-09 16:08:12 +0600

    pdf template 1st page created

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{html,js}' // Make sure to include the src directory for proper class scanning
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
